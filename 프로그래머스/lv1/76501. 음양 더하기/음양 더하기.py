325d7be018b4e74065b55adc5083acb8436bd664
JeeyeonKim00 <127364024+JeeyeonKim00@users.noreply.github.com>
2023-08-21 17:04:58 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,7 +1,4 @@
 def solution(absolutes, signs):
-    signs_modified = ['-' if i == False else '+' for i in signs ]
-    fin_num = 0
-    for num, pm in zip(absolutes, signs_modified):
-        fin_num += int(pm+str(num))
-    
-    return fin_num
+  
+    return sum([-ab if si == False else +ab for ab,si in zip(absolutes, signs)])
+
