cd107e0f3aabf75ee7a34a6d86e0ba5fce49cfa7
JeeyeonKim00 <127364024+JeeyeonKim00@users.noreply.github.com>
2023-10-25 11:08:33 +0900

    [level 1] Title: 최소직사각형, Time: 3.21 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,14 +1,17 @@
+# def solution(sizes):
+# #     for i in sizes:
+# #         if i[0] < i[1]:
+# #             i[0],i[1] = i[1], i[0]
+
+# #     width = []
+# #     height = []
+
+# #     for i in sizes:
+# #         width.append(i[0])
+# #         height.append(i[1])
+
+# #     answer = max(width)*max(height)
+#     return answer
+
 def solution(sizes):
-    for i in sizes:
-        if i[0] < i[1]:
-            i[0],i[1] = i[1], i[0]
-
-    width = []
-    height = []
-
-    for i in sizes:
-        width.append(i[0])
-        height.append(i[1])
-
-    answer = max(width)*max(height)
-    return answer
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
